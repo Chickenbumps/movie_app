726a184590f8f6e1998f12c9960f26a4912a7e93
Chickenbumps <whgotjd46@gmail.com>
2019-03-11 23:29:50 +0900

    Updates

---
+++ precache-manifest.6f0b666ef94d820cf21f5a28d680c8b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "29efa5b095d41fb528d3",
+    "url": "/movie_app/static/css/main.baf162e6.chunk.css"
+  },
+  {
+    "revision": "29efa5b095d41fb528d3",
+    "url": "/movie_app/static/js/main.b3d8a135.chunk.js"
+  },
+  {
+    "revision": "7952d9d4b252ca1c580e",
+    "url": "/movie_app/static/js/runtime~main.e6fc6960.js"
+  },
+  {
+    "revision": "bc535225cb436567ca47",
+    "url": "/movie_app/static/js/2.dc831639.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/movie_app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "675bf02770c2bc10c5953602224a33ed",
+    "url": "/movie_app/index.html"
+  }
+];
